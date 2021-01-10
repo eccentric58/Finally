1d2dd7ba97a715f7f4aa53ea1d0e02afd8113f56
Igor Zhosan <zhosan00@list.ru>
2021-01-10 14:52:05 +0300

    adding files

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,30 @@
+class ApplicationController < ActionController::Base
+  helper_method :current_user
+  before_action :set_locale
+
+  def set_locale
+    session[:locale] = params[:locale] if params[:locale]
+    I18n.locale = session[:locale] || I18n.default_locale
+  end
+
+  def current_user
+    return @current_user if @current_user.present?
+
+    if session[:user_id].present?
+      @current_user = User.find_by(id: session[:user_id])
+    else
+      @current_user = User.generate
+      session[:user_id] = @current_user.id
+      @current_user
+    end
+
+    if @current_user.nil?
+      @current_user = User.generate
+      session[:user_id] = @current_user.id
+      @current_user
+    end
+
+    @current_user
+  end
+
+end
